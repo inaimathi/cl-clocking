0f8a3c438bff2418fd56fb2225099509fb7b1636
inaimathi <leo.zovic@gmail.com>
2018-08-13 20:12:57 -0400

    Working TCP tests

--- cl-clocking.lisp
+++ cl-clocking.lisp
@@ -2,27 +2,45 @@
 
 (in-package #:cl-clocking)
 
+(defparameter *generator* (session-token:make-generator :token-length 1000))
+(defun gen! () (funcall *generator*))
+
 (defun sha256 (str)
   (ironclad:byte-array-to-hex-string
    (ironclad:digest-sequence
     :sha256 (ironclad:ascii-string-to-byte-array str))))
 
-(defun tcp-send (sock input)
-  nil)
+(defun tcp-send (host port input)
+  (let* ((sock (usocket:socket-connect host port))
+         (stream (usocket:socket-stream sock)))
+    (format stream "~a" input)
+    (force-output stream)
+    (read-sequence (make-string 2) stream)
+    (usocket:socket-close sock)))
 
-(defun tcp-receive (sock)
-  nil)
+(defun tcp-receive (server)
+  (let* ((sock (usocket:socket-accept server))
+         (stream (usocket:socket-stream sock))
+         (buf (make-string 1000)))
+    (read-sequence buf stream)
+    (format stream "~a" "ok")
+    (force-output stream)
+    (usocket:socket-close sock)
+    buf))
 
 (defun battery-status ()
   (loop for b in (trivial-battery:battery-info)
      collect (trivial-battery:battery-details
               (cdr (assoc "name" b :test #'string=)))))
 
-(defun profile! (&key (times 1000))
-  (let* ((gen (session-token:make-generator :token-length 1000))
-         (server (usocket:socket-listen usocket:*wildcard-host* 8080 :reuse-address t))
+(defun profile! (&key (times 1) (port 8080))
+  (let* ((server (usocket:socket-listen usocket:*wildcard-host* port :reuse-address t))
+         (server-thread (bt:make-thread (lambda () (loop (tcp-receive server)))))
          (before (battery-status)))
     (loop repeat times
-       do (let ((inp (funcall gen)))
-            (sha256 inp)))
+       do (let ((inp (gen!)))
+            (sha256 inp)
+            (tcp-send "localhost" port inp)))
+    (usocket:socket-close server)
+    (bt:destroy-thread server-thread)
     (list :before before :after (battery-status))))
